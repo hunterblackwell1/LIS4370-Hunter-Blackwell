09f39bab317f6a462998a719d2a88a4d149a7de5
hunterblackwell1 <112523398+hunterblackwell1@users.noreply.github.com>
2023-01-19 20:38:17 -0500

    Add files via upload

---
+++ Module #2 Assignment.R
@@ -0,0 +1,8 @@
+# Create the assignment2 vector
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+# Create myMean function
+myMean <- function(assignment2) { return(sum(assignment2)/length(assignment2)) }
+
+# Evaluate mean of assignment2
+myMean(assignment2)
