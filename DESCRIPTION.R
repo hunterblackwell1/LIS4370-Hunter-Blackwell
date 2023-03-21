39ff0f5309f2272b78d9147caf0c5e5a46c30d14
hunterblackwell1 <112523398+hunterblackwell1@users.noreply.github.com>
2023-03-20 20:45:13 -0400

    Add files via upload

---
+++ DESCRIPTION.R
@@ -0,0 +1,11 @@
+Package: blackwelltestpackage
+Type: Package
+Title: This package will be used in my final project.
+Version: 0.0.0.9000
+Author: "Hunter Blackwell <blackwell.h@usf.edu> [aut, cre]"
+Maintainer: Hunter Blackwell  <blackwell.h@usf.edu>
+Description: This package contains functions that can be used for data analysis 
+    on a particular data set that has yet to be decided. 
+License: CC0
+Encoding: UTF-8
+LazyData: true
